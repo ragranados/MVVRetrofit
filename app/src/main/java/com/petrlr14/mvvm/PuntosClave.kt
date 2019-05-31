4b0093c98400ba983078f2f6b9470db8463b705d
ragranados <37202487+ragranados@users.noreply.github.com>
2019-05-31 10:49:40 -0600

    Create PuntosClave.kt

---
+++ app/src/main/java/com/petrlr14/mvvm/PuntosClave.kt
@@ -0,0 +1,9 @@
+/*
+- crear interfaz
+- @GET("/users/{users}/repos")
+- fun getRepos(@Path("user") user: String): Deferred <Response<List<GitHubRepo>>>
+- hacer URL base
+
+- se debe usar anotacion @field: Json(name=<nombreQueVieneDeLaApi>)
+- hacer singleton
+*/
